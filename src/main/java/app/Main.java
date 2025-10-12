924307a93631a994b5ca553b620eccbfcf2acd07
phllp <beigerfelipe@gmail.com>
2025-10-12 11:31:53 -0300

    feat: implement single car moving across the grid

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,12 +1,19 @@
 package app;
 
+import app.model.enums.LaneCodes;
 import app.view.MatrixCanvas;
 import app.view.Ui;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.scene.control.Button;
+import javafx.scene.control.Spinner;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import utils.MatrixParser;
 
 import java.io.InputStream;
+import java.util.ArrayList;
 
 public class Main extends Application {
 
@@ -14,6 +21,10 @@ public class Main extends Application {
 
     /** Armazena a malha */
     private int[][] gridRef;
+
+    /** Testes com um carro */
+    private Timeline demo;
+    private int carR = -1, carC = -1;
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -27,7 +38,94 @@ public class Main extends Application {
 
         Ui ui = new Ui();
         ui.buildLayout(stage, matrixCanvas);
+        addActionListeners(ui);
     }
+
+    private void addActionListeners(Ui ui) {
+        Button btnIniciar = ui.getBtnIniciar();
+        Button btnEncerrar = ui.getBtnEncerrar();
+        Spinner<Integer> spnIntervaloMs = ui.getSpnIntervaloMs();
+
+        btnIniciar.setOnAction(event -> {
+            startStraightRightDemo(spnIntervaloMs.getValue());
+        });
+
+        btnEncerrar.setOnAction(e -> {
+            stopDemo();
+            matrixCanvas.clearCar();
+        });
+    }
+
+    /**
+     * Encontra a primeira entrada na parte da malha,
+     * para movimentar da esquerda para a direita
+     * @return
+     */
+    private int[] findFirstRightLaneSegment() {
+        for (int r = 0; r < gridRef.length; r++) {
+            int c = 0;
+            while (c < gridRef[r].length) {
+                // pula até achar um '2'
+                while (c < gridRef[r].length && gridRef[r][c] != 2) c++;
+                if (c >= gridRef[r].length) break;
+
+                int start = c;
+
+                ArrayList<Integer> crossroads = new ArrayList<>();
+                crossroads.add(LaneCodes.CRUZAMENTO_CIMA.getCodigo());
+                crossroads.add(LaneCodes.CRUZAMENTO_BAIXO.getCodigo());
+                crossroads.add(LaneCodes.CRUZAMENTO_CIMA_DIREITA.getCodigo());
+                crossroads.add(LaneCodes.CRUZAMENTO_ESQUERDA_BAIXO.getCodigo());
+                crossroads.add(LaneCodes.CRUZAMENTO_DIREITA_BAIXO.getCodigo());
+                crossroads.add(LaneCodes.CRUZAMENTO_CIMA_ESQUERDA.getCodigo());
+
+
+                while (c < gridRef[r].length && (gridRef[r][c] == 2 || crossroads.contains(gridRef[r][c]))) c++;
+                int end = c - 1;
+
+                // achamos um segmento [start..end] de via 2
+                return new int[]{ r, start, end };
+            }
+        }
+        return null;
+    }
+
+    private void startStraightRightDemo(int stepMs) {
+        stopDemo(); // garante que não tem outro rodando
+
+        int[] seg = findFirstRightLaneSegment();
+        if (seg == null) {
+            System.out.println("Nenhum segmento horizontal de '2' encontrado.");
+            return;
+        }
+        carR = seg[0];
+        carC = seg[1];
+        matrixCanvas.setCar(carR, carC);
+
+        int endC = seg[2];
+
+        System.out.println("End = " + endC);
+
+        demo = new Timeline(new KeyFrame(Duration.millis(stepMs), e -> {
+            if (carC < endC) {
+                carC += 1;
+                matrixCanvas.setCar(carR, carC);
+            } else {
+                // chegou no fim do segmento -> para a demo
+                stopDemo();
+            }
+        }));
+        demo.setCycleCount(Timeline.INDEFINITE);
+        demo.play();
+    }
+
+    private void stopDemo() {
+        if (demo != null) {
+            demo.stop();
+            demo = null;
+        }
+    }
+
 
     private int[][] loadGridFromResources(String resourcePath) throws Exception {
         try (InputStream in = getClass().getResourceAsStream(resourcePath)) {
